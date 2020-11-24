75e715e720b4e9785aa9368b6bc6e97e86fc2c62
Andrii Chornyi <chorny.andriy@gmail.com>
2020-11-25 00:46:52 +0200

    more better

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Flask is running much better'
+    return 'Flask is running much better. And even more better!!!'
 
 if __name__ == '__main__':
     app.run()
