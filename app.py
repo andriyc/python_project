83ad653077e2dae239a40199c9f4bdd90bdf9f5b
Andrii Chornyi <chorny.andriy@gmail.com>
2020-11-25 01:11:10 +0200

    CD to DEV is working well. Lets go on

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Flask is running much better. And even more better!!!'
+    return 'CD to DEV is working well.'
 
 if __name__ == '__main__':
     app.run()
