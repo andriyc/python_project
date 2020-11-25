33f7fde67154bfb76dd79d1c2a50e7ba769de2f5
Andrii Chornyi <chorny.andriy@gmail.com>
2020-11-25 22:42:28 +0200

    testing unittest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,6 @@
 import unittest
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import app
+from .context import app
 
 
 def test_app():
