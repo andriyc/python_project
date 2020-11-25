2d7c07ee6a72095128dfcdbe9ee938b174a6b818
Andrii Chornyi <chorny.andriy@gmail.com>
2020-11-25 02:31:44 +0200

    testing unittest

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_project',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='User',
+    author_email='',
+    description=''
+)
