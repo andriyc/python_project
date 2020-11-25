eb19a0f1ba15cc660042b6635176faaab95a9c6c
Andrii Chornyi <chorny.andriy@gmail.com>
2020-11-25 22:20:43 +0200

    testing unittest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,12 @@
 import unittest
-#import app
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import app
 
 
 def test_app():
-    assert True  #app.index() == 'CI/CD to DEV is working well !!! Powered by Jenkins!!!'
-
+    assert app.index() == 'CI/CD to DEV is working well !!! Powered by Jenkins!!!'
