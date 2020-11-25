4cf6823aa5c4b2d1dd04a02045c6a9cd95aed980
Andrii Chornyi <chorny.andriy@gmail.com>
2020-11-25 17:28:18 +0200

    testing unittest

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,5 +3,5 @@ import unittest
 
 
 def test_app():
-    assert True #app.index() == 'CI/CD to DEV is working well !!! Powered by Jenkins!!!'
+    assert True  #app.index() == 'CI/CD to DEV is working well !!! Powered by Jenkins!!!'
 
