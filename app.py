b017a2aaaa1945668bbcd6c04f63095c47909163
Andrii Chornyi <chorny.andriy@gmail.com>
2020-11-26 20:34:33 +0200

    implemented config.ini

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, jsonify, render_template
-
-DEBUG_MODE = False;
+import configparser
 
 app = Flask(__name__)
 
+
+def init_globals() -> configparser:
+    config = configparser.ConfigParser()
+    config.read('settings/config.ini')
+    return config
+
+
+config = init_globals()
+
+
 @app.route('/')
 def index():
-    return render_template("index.html", message='CI/CD to DEV is working well !!! Powered by Jenkins!!!')
+    return render_template("index.html", message=config['MESSAGE']['CI_CD_WORKING'])
 
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG_MODE)
+    app.run(debug=(config['APP']['DEBUG_MODE'].lower() == 'true'))
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,9 +2,10 @@ import unittest
 
 from context import app
 
+
 class AppTests(unittest.TestCase):
     def test_app(self):
-        assert True #app.index() == 'CI/CD to DEV is working well !!! Powered by Jenkins!!!'
+        assert True  # app.index() == 'CI/CD to DEV is working well !!! Powered by Jenkins!!!'
 
     def test_if_debug_mode(self):
         assert not app.DEBUG_MODE, 'Deploy in debug mode is permitted!!!'
